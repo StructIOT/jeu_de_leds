dc6bee327a8e3a07d2b7c9f003bdb98eda67f38d
Yamiko <yamikohema@gmail.com>
2025-02-23 07:49:18 +0000

    refactor: renommer la variable  en .

--- jeu_de_leds.ino
+++ jeu_de_leds.ino
@@ -1,24 +1,24 @@
 // Définition des trois leds
 const int LED_ROUGE = 10;
 const int LED_JAUNE = 9;
-const int LED_VERT = 8;
+const int LED_VERTE = 8;
 
 void setup() {
   //Configuration des broches en sortie
   pinMode(LED_JAUNE, OUTPUT);
   pinMode(LED_JAUNE, OUTPUT);
-  pinMode(LED_VERT, OUTPUT);
+  pinMode(LED_VERTE, OUTPUT);
 
   //Initialisation des leds à l'état bas
   digitalWrite(LED_ROUGE, LOW);
   digitalWrite(LED_JAUNE, LOW);
-  digitalWrite(LED_VERT,LOW);
+  digitalWrite(LED_VERTE,LOW);
 }
 
 void loop() {
-  digitalWrite(LED_VERT, HIGH);
+  digitalWrite(LED_VERTE, HIGH);
   delay(3000);
-  digitalWrite(LED_VERT, LOW);
+  digitalWrite(LED_VERTE, LOW);
   digitalWrite(LED_JAUNE, HIGH);
   delay(1500);
   digitalWrite(LED_JAUNE, LOW);
